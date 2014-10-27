abb508476526aadd6c70e0abe2cce19083444a75
grimonprez <grimonprez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-27 10:19:45 +0000

    - reduce example duration
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpalars@139 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/HDPenReg/R/LarsPath.R
+++ pkg/HDPenReg/R/LarsPath.R
@@ -160,7 +160,7 @@ setMethod(
 #' @param xlab Name of the x axis.
 #' @param ... Other plot arguments.
 #' @examples 
-#' dataset=simul(50,10000,0.4,10,50,matrix(c(0.1,0.8,0.02,0.02),nrow=2))
+#' dataset=simul(50,1000,0.4,10,50,matrix(c(0.1,0.8,0.02,0.02),nrow=2))
 #' result=HDfusion(dataset$data,dataset$response) 
 #' plotCoefficient(result,result@@nbStep) #plot coefficients at the last step
 #' @export 
@@ -213,7 +213,7 @@ plotCoefficient=function(x,step,ylab="coefficients",xlab="variables",...)
 #' @param step The step at which you want to get the coefficients.
 #' @return a vector of size p containing the value of coefficients at the desired step.
 #' @examples 
-#' dataset=simul(50,10000,0.4,10,50,matrix(c(0.1,0.8,0.02,0.02),nrow=2))
+#' dataset=simul(50,1000,0.4,10,50,matrix(c(0.1,0.8,0.02,0.02),nrow=2))
 #' result=HDfusion(dataset$data,dataset$response)
 #' coefficient=coeff(result,result@@nbStep) #get the coefficients
 #' @export 
